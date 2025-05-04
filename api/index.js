9766fe5a7cdc6f15efb5ea9f0f55a853e2df899d
MatthewImmanuel <noels170rus2005@gmail.com>
2025-05-04 22:51:37 +0700

    change env

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const Todo = require('./models/TodoSchema');
+const Todo = require('../models/TodoSchema');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -62,4 +62,3 @@ app.delete('/deleteTodo/:id', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => console.log(`🚀 Server started at port: ${PORT}`));
