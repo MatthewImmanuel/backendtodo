793b02f6de0466c33d4a9ea163709c0f9506b48d
MatthewImmanuel <noels170rus2005@gmail.com>
2025-05-04 22:21:39 +0700

    Initial backend commit

---
+++ models/TodoSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const TodoSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: [true, 'Task is required']
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
